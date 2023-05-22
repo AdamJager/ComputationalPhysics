f820c8192faca7910ed789b051f842634db04bd6
AdamJager <102496837+AdamJager@users.noreply.github.com>
2023-05-22 16:05:36 +0100

    Delete getAccuracy.m

--- getAccuracy.m
+++
@@ -1,6 +0,0 @@
-function [accuracy] = getAccuracy(labelGuesses, labels)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    difference = labelGuesses- labels;
-    accuracy = sum(difference(:) == 0) / length(difference) * 100;
-end
