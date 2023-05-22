efd5003a67b29dd9c7065a2ba80728e3a85dc836
AdamJager <102496837+AdamJager@users.noreply.github.com>
2023-05-22 16:11:15 +0100

    Delete ExtremeLearningMachine directory

--- ExtremeLearningMachine/extractClasses.m
+++
@@ -1,7 +0,0 @@
-function [twoClassData,twoClassLabels] = extractClasses(data, labels, class1, class2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    indexes = find(labels == class1 | labels == class2);
-    twoClassData = data(indexes, :);
-    twoClassLabels = (labels(indexes) == class1);
-end
--- ExtremeLearningMachine/outputWeights.m
+++
@@ -1,5 +0,0 @@
-function [outputWeights, initialWeights, biases] = outputWeights(xTrain, labels, inputLayerSize, hiddenLayerSize, alpha)
-    initialWeights = normrnd(0, 0.5, inputLayerSize, hiddenLayerSize);
-    biases = normrnd(0, 0.5, hiddenLayerSize, 1)';
-    outputWeights = pinv(hiddenNodes(xTrain, initialWeights, biases, alpha)) * labels;
-end
